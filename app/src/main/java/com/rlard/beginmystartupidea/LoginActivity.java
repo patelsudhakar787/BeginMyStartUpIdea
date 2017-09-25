c1aa34ba88a69b6d65ae2dc417108fd2d53aeecd
patelsudhakar787 <patelsudhakar787@gmail.com>
2017-09-25 12:17:45 +0530

    commited

--- app/src/main/java/com/rlard/beginmystartupidea/LoginActivity.java
+++ app/src/main/java/com/rlard/beginmystartupidea/LoginActivity.java
@@ -3,6 +3,7 @@ package com.rlard.beginmystartupidea;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+
+
+        Toast.makeText(LoginActivity.this,"Hello",Toast.LENGTH_LONG).show();
     }
 }
